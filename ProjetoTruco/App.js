5c226608de56127f05849dcee563f4790f497475
otaviobleich <ferreiraotavio577@gmail.com>
2026-03-17 20:12:51 -0300

    feat: adiciona botões de pontuação do truco (3,6,9,12)

--- ProjetoTruco/App.js
+++ ProjetoTruco/App.js
@@ -4,38 +4,37 @@ import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
 
 export default function App() {
 
-  const [nos, setNos] = useState(10)
-  const [eles, setEles] = useState(10)
+  const [nos, setNos] = useState(0)
+  const [eles, setEles] = useState(0)
 
-  function aumentarNos(){
-    setNos(nos + 1)
+  function aumentarNos(){ setNos(nos + 1) }
+  function diminuirNos(){ 
+    if
+    (nos > 0){
+      setNos(nos - 1)
+    }
   }
 
-  function diminuirNos(){
-    setNos(nos - 1)
-  }
-
-  function aumentarEles(){
-    setEles(eles + 1)
-  }
-
-  function diminuirEles(){
-    setEles(eles - 1)
+  function aumentarEles(){ setEles(eles + 1) }
+  function diminuirEles(){ 
+    if(eles > 0){
+setEles(eles - 1) 
+    }
+    
   }
 
   return (
     <View style={styles.container}>
 
       <Image
-        source={{uri:'https://upload.wikimedia.org/wikipedia/commons/5/5e/Unipar_logo.png'}}
-        style={styles.logo}
+        source={require('./assets/download-removebg-preview.png')}
       />
 
       <View style={styles.placar}>
 
         {/* NÓS */}
         <View style={styles.time}>
-          <Text style={styles.titulo}>NÓS</Text>
+          <Text style={styles.titulo}>Nois</Text>
           <Text style={styles.numero}>{nos}</Text>
 
           <View style={styles.botoes}>
@@ -47,11 +46,17 @@ export default function App() {
               <Text style={styles.textoBotao}>-</Text>
             </TouchableOpacity>
           </View>
+
+          {/* BOTÕES EXTRAS */}
+          <TouchableOpacity style={styles.truco}><Text style={styles.txt}>TRUCO</Text></TouchableOpacity>
+          <TouchableOpacity style={styles.seis}><Text style={styles.txt}>SEISSS</Text></TouchableOpacity>
+          <TouchableOpacity style={styles.nove}><Text style={styles.txt}>NOVEEE</Text></TouchableOpacity>
+          <TouchableOpacity style={styles.doze}><Text style={styles.txt}>DOZI</Text></TouchableOpacity>
         </View>
 
         {/* ELES */}
         <View style={styles.time}>
-          <Text style={styles.titulo}>ELES</Text>
+          <Text style={styles.titulo}>Eles</Text>
           <Text style={styles.numero}>{eles}</Text>
 
           <View style={styles.botoes}>
@@ -63,6 +68,12 @@ export default function App() {
               <Text style={styles.textoBotao}>-</Text>
             </TouchableOpacity>
           </View>
+
+          {/* BOTÕES EXTRAS */}
+          <TouchableOpacity style={styles.truco}><Text style={styles.txt}>TRUCO</Text></TouchableOpacity>
+          <TouchableOpacity style={styles.seis}><Text style={styles.txt}>SEISSS</Text></TouchableOpacity>
+          <TouchableOpacity style={styles.nove}><Text style={styles.txt}>NOVEEE</Text></TouchableOpacity>
+          <TouchableOpacity style={styles.doze}><Text style={styles.txt}>DOZI</Text></TouchableOpacity>
         </View>
 
       </View>
@@ -80,12 +91,11 @@ const styles = StyleSheet.create({
     alignItems:'center',
     backgroundColor:'#f2f2f2'
   },
-
   logo:{
     width:120,
     height:60,
     resizeMode:'contain',
-    marginBottom:40
+    marginBottom:20
   },
 
   placar:{
@@ -94,45 +104,87 @@ const styles = StyleSheet.create({
 
   time:{
     alignItems:'center',
-    marginHorizontal:20
+    marginHorizontal:15
   },
 
   titulo:{
-    fontSize:18,
-    fontWeight:'bold',
-    color:'#666'
+    fontSize:20,
+    fontWeight: 'regular',
+    color:'#000000'
   },
 
   numero:{
     fontSize:60,
     fontWeight:'bold',
-    marginVertical:20
+    marginVertical:10
   },
 
   botoes:{
-    flexDirection:'row'
+    flexDirection:'row',
+    marginBottom:10
   },
 
   botaoMais:{
-    backgroundColor:'#0f5f3d',
-    padding:15,
+    backgroundColor:'#08ee1b',
+    padding:10,
     borderRadius:10,
     marginRight:10,
-    width:60,
+    width:50,
     alignItems:'center'
   },
 
   botaoMenos:{
-    backgroundColor:'#8b0024',
-    padding:15,
+    backgroundColor:'#ff0000',
+    padding:10,
     borderRadius:10,
-    width:60,
+    width:50,
     alignItems:'center'
   },
 
   textoBotao:{
     color:'#fff',
-    fontSize:22,
+    fontSize:20,
+    fontWeight:'bold'
+  },
+
+  /* BOTÕES GRANDES */
+  truco:{
+    backgroundColor:'#58b14c',
+    width:120,
+    padding:10,
+    borderRadius:10,
+    marginBottom:8,
+    alignItems:'center'
+  },
+
+  seis:{
+    backgroundColor:'#1f3c88',
+    width:120,
+    padding:10,
+    borderRadius:10,
+    marginBottom:8,
+    alignItems:'center'
+  },
+
+  nove:{
+    backgroundColor:'#16b5d1',
+    width:120,
+    padding:10,
+    borderRadius:10,
+    marginBottom:8,
+    alignItems:'center'
+  },
+
+  doze:{
+    backgroundColor:'#7f0000',
+    width:120,
+    padding:10,
+    borderRadius:10,
+    alignItems:'center'
+  },
+
+  txt:{
+    color:'#fff',
     fontWeight:'bold'
   }
 
